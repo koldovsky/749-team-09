a34e6d75f51416ac5259eb542e94757a4e755cf6
Nazar Petryshyn <petryshynazary@gmail.com>
2023-02-27 21:29:42 +0200

    added js

--- js/main/whychooseus/again.js
+++ js/main/whychooseus/again.js
@@ -1,14 +1,40 @@
-const recommendedServicesItems = document.querySelectorAll('.whychooseus__recommended-services-item');
+(function () {
 
-recommendedServicesItems.forEach((item) => {
-  item.addEventListener('mousedown', (event) => {
-    
-    const rect = item.getBoundingClientRect();
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
-    const translateX = centerX - rect.left - rect.width / 2;
-    const translateY = centerY - rect.top - rect.height / 2;
+  const recommendedServicesItems = document.querySelectorAll('.whychooseus__recommended-services-item');
+  let selectedItem = null;
 
-    item.style.transform = `translate(${translateX}px, ${translateY}px) scale(2)`;
+  recommendedServicesItems.forEach((item) => {
+    item.addEventListener('mousedown', (event) => {
+      if (selectedItem === item) {
+        selectedItem.classList.remove('is-scaled');
+        selectedItem.style.transition = 'transform 0.5s ease-out';
+        selectedItem.style.transform = 'none';
+        setTimeout(() => {
+          selectedItem.style.transition = '';
+          selectedItem = null;
+        }, 500);
+      } else {
+        if (selectedItem) {
+          selectedItem.classList.remove('is-scaled');
+          selectedItem.style.transition = 'transform 0.5s ease-out';
+          selectedItem.style.transform = 'none';
+          setTimeout(() => {
+            selectedItem.style.transition = '';
+          }, 500);
+        }
+        selectedItem = item;
+        selectedItem.classList.add('is-scaled');
+        const rect = selectedItem.getBoundingClientRect();
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+        const translateX = centerX - rect.left - rect.width / 2;
+        const translateY = centerY - rect.top - rect.height / 2;
+        selectedItem.style.transition = 'transform 0.5s ease-out';
+        selectedItem.style.transform = `translate(${translateX}px, ${translateY}px) scale(3.5)`;
+        setTimeout(() => {
+          selectedItem.style.transition = '';
+        }, 500);
+      }
+    });
   });
-});
+})();
