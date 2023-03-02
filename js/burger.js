6fabfa693ac1e87b457d992b338661ccad5c588e
Valentyn Zatserkovnyi <91143634+Valentyn23@users.noreply.github.com>
2023-03-02 01:43:56 +0000

    add animation to burger header and smooth #

--- js/burger.js
+++ js/burger.js
@@ -1,21 +1,23 @@
 (function () {
-    const iconBurger = document.querySelector(".header__nav-burger");
-    const navigationsBody = document.querySelector(".header__menu");
-    iconBurger.addEventListener("click", function (e) {
-      document.body.classList.toggle("lock");
-      iconBurger.classList.toggle("active");
-      navigationsBody.classList.toggle("active");
-    });
-  
-    const navLinks = document.querySelectorAll(".header__menu-item");
-    navLinks.forEach((navLink) => {
-      navLink.addEventListener("click", (_) => {
-            if (iconBurger.classList.contains("active")) {
-                document.body.classList.remove("lock");
-                iconBurger.classList.remove("active");
-                navigationsBody.classList.remove("active");
-            }
-          }
-      );
-    });
-  })();
+  const iconBurger = document.querySelector(".header__nav-burger");
+  const navigationsBody = document.querySelector(".header__menu");
+
+  iconBurger.addEventListener("click", function (e) {
+    document.body.classList.toggle("lock");
+    iconBurger.classList.toggle("active");
+    navigationsBody.classList.toggle("active");
+  });
+
+
+  const navLinks = document.querySelectorAll(".header__menu-item");
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (_) => {
+      if (iconBurger.classList.contains("active")) {
+        document.body.classList.remove("lock");
+        iconBurger.classList.remove("active");
+        navigationsBody.classList.remove("active");
+      }
+    }
+    );
+  });
+})();
