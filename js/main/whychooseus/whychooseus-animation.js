7852a6f4f8b9cbaa851946de2e1c6c4218569669
Nazar Petryshyn <petryshynazary@gmail.com>
2023-03-02 14:24:24 +0200

    added js

--- js/main/whychooseus/whychooseus-animation.js
+++ js/main/whychooseus/whychooseus-animation.js
@@ -1,40 +1,24 @@
 (function () {
 
-  const recommendedServicesItems = document.querySelectorAll('.whychooseus__recommended-services-item');
-  let selectedItem = null;
+  const services = document.querySelectorAll('.whychooseus__recommended-services-item');
 
-  recommendedServicesItems.forEach((item) => {
-    item.addEventListener('mousedown', () => {
-      if (selectedItem === item) {
-        selectedItem.classList.remove('is-scaled');
-        selectedItem.style.transition = 'transform 0.3s ease-out';
-        selectedItem.style.transform = 'none';
-        setTimeout(() => {
-          selectedItem.style.transition = '';
-          selectedItem = null;
-        }, 500);
-      } else {
-        if (selectedItem) {
-          selectedItem.classList.remove('is-scaled');
-          selectedItem.style.transition = 'transform 0.3s ease-out';
-          selectedItem.style.transform = 'none';
-          setTimeout(() => {
-            selectedItem.style.transition = '';
-          }, 500);
+  services.forEach((service, index) => {
+    service.addEventListener('click', () => {
+
+      const isServiceActive = service.classList.contains("active");
+
+      services.forEach((otherService, otherIndex) => {
+
+        otherService.classList.remove('active');
+        otherService.classList.remove('is-inactive');
+
+        if (!isServiceActive && index !== otherIndex) {
+          otherService.classList.add("is-inactive");
         }
-        selectedItem = item;
-        selectedItem.classList.add('is-scaled');
-        const rect = selectedItem.getBoundingClientRect();
-        const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight / 2;
-        const translateX = centerX - rect.left - rect.width / 2;
-        const translateY = centerY - rect.top - rect.height / 2;
-        selectedItem.style.transition = 'transform 0.3s ease-out';
-        selectedItem.style.transform = `translate(${translateX}px, ${translateY}px) scale(2)`;
-        setTimeout(() => {
-          selectedItem.style.transition = '';
-        }, 500);
-      }
+      });
+
+      if (!isServiceActive) service.classList.add("active");
     });
   });
+
 })();
--- js/main/whychooseus/whychooseus__recommended-services-item.js
+++ js/main/whychooseus/whychooseus__recommended-services-item.js
@@ -3,27 +3,33 @@
     const services = [
         {
             svgPicture: 'img/main_whychooseus/cateredmeals.svg',
-            name: 'Catered Meals'
+            name: 'Catered Meals',
+            description: 'Our catering service offers delicious, customized meals for any occasion. From corporate events to intimate gatherings, we provide a wide range of menu options that can be tailored to your specific needs and preferences. Our experienced team of chefs and staff ensure that your guests will be satisfied and impressed with our high-quality cuisine and impeccable service'
         },
         {
             svgPicture: 'img/main_whychooseus/conferencerooms.svg',
-            name: 'Conference Rooms'
+            name: 'Conference Rooms',
+            description: 'Our catering service offers delicious, customized meals for any occasion. From corporate events to intimate gatherings, we provide a wide range of menu options that can be tailored to your specific needs and preferences. Our experienced team of chefs and staff ensure that your guests will be satisfied and impressed with our high-quality cuisine and impeccable service'
         },
         {
             svgPicture: 'img/main_whychooseus/community.svg',
-            name: 'Community'
+            name: 'Community',
+            description: 'Our catering service offers delicious, customized meals for any occasion. From corporate events to intimate gatherings, we provide a wide range of menu options that can be tailored to your specific needs and preferences. Our experienced team of chefs and staff ensure that your guests will be satisfied and impressed with our high-quality cuisine and impeccable service'
         },
         {
             svgPicture: 'img/main_whychooseus/eventsandlearnig.svg',
-            name: 'Events & Learning'
+            name: 'Events & Learning',
+            description: 'Our catering service offers delicious, customized meals for any occasion. From corporate events to intimate gatherings, we provide a wide range of menu options that can be tailored to your specific needs and preferences. Our experienced team of chefs and staff ensure that your guests will be satisfied and impressed with our high-quality cuisine and impeccable service'
         },
         {
             svgPicture: 'img/main_whychooseus/working24per7.svg',
-            name: 'Working 24/7'
+            name: 'Working 24/7',
+            description: 'Our catering service offers delicious, customized meals for any occasion. From corporate events to intimate gatherings, we provide a wide range of menu options that can be tailored to your specific needs and preferences. Our experienced team of chefs and staff ensure that your guests will be satisfied and impressed with our high-quality cuisine and impeccable service'
         },
         {
             svgPicture: 'img/main_whychooseus/highspeedinternet.svg',
-            name: 'High-Speed Internet'
+            name: 'High-Speed Internet',
+            description: 'Our catering service offers delicious, customized meals for any occasion. From corporate events to intimate gatherings, we provide a wide range of menu options that can be tailored to your specific needs and preferences. Our experienced team of chefs and staff ensure that your guests will be satisfied and impressed with our high-quality cuisine and impeccable service'
         },
     ];
 
@@ -42,6 +48,9 @@
                     </div>
                     <div class="whychooseus__recommended-services-item-service-text">
                          <h3>${service.name}</h3>
+                    </div>
+                    <div class="whychooseus__recommended-services-item-service-description">
+                         <p>${service.description}</p>
                     </div>
                 </div>
             </div>
